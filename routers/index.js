cf631ebacdfa62bd069cdbb1a0bcdef612faf9b8
thaivm-0609 <vuongthai95@gmail.com>
2024-11-23 18:10:03 +0700

    feat: signup signin

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import bookRouter from "./books.js";
 import apiRouter from "./api.js";
+import authRouter from "./auth.js";
 
 const router = Router(); 
 
 router.use('/', bookRouter); //router trả về giao diện ejs
 router.use('/api', apiRouter); //router trả về api (dữ liệu dạng json)
+router.use('/auth', authRouter); //router đăng ký/đăng nhập
 
 export default router;
